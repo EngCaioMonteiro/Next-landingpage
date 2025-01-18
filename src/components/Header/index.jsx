76ecf11d46e200954090df9f2579b0fc2d598788
Caio Oliveira Monteiro <engcaiomonteiro@gmail.com>
2025-01-18 19:43:04 -0300

    Inicio do projeto e criação da estrutura base para a evolução de uma landing page com Next js

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import logo from '../../../public/images/logo.svg';
+import Link from 'next/link';
+import Styles from "./index.module.scss"
+
+const Header = () => {
+    return(
+        <div className={Styles.container}>
+            <div className={Styles.logotipo}>
+                <Image src={logo}></Image>
+            </div>
+            <div className={Styles.menu}>
+                <Link href="/">Home</Link>                    
+                <Link href="/">O que fazemos</Link>                    
+                <Link href="/">Cases</Link>                
+            </div>
+            <div className={Styles.action}>
+                <button className={Styles.button}>Fale Conosco</button>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
